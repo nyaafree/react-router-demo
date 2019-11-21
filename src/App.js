042f2cacacf81d804a08e7485af14a5c17d9e7c4
nyaafree <sumedicecaless.7777@gmail.com>
2019-11-21 13:52:40 +0900

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+// Switch,Routeコンポーネントを読み込む
+import { Switch,Route } from 'react-router-dom';
 
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-    <div className='App'>
-      <HomePage/>
+    <div>
+      {/* <HomePage/> */}
+      {/* <Route/>コンポーネントにexact属性をつけないと'/hats'にアクセスされても'/hats’に指定したコンポーネントのみならず、
+      '/'に指定したコンポーネントも表示されてしまう。 */}
+      {/* Switchコンポーネントの中にRouteコンポーネントを入れておくと現在のパスと適合するRouteコンポーネントのみを表示してくれる。 */}
+      <Switch>
+        <Route exact  path='/' component={ HomePage }/>
+        <Route exact path='/hats' component={ HatsPage }/>
+      </Switch>
+      
     </div>
     )
   }
